0619a4034636e9c581d380d41dfa07fac2b499ad
GregC-31 <gtac253@gmail.com>
2026-04-28 14:29:36 -0700

    Add Microsoft.Data.SqlClient package

--- api/obj/Debug/net10.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net10.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc7d9b6af480952df169fbacfbda40cc09890fa9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce14f5c29881de0bfa1ad880ae107b4b813e512d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
