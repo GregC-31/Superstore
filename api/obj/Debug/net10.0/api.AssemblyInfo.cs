8adcb4d1d57f36eb9598a6f8302745a509442b09
GregC-31 <gtac253@gmail.com>
2026-04-30 14:13:55 -0700

    Changes on startup of .cs programs

--- api/obj/Debug/net10.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net10.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce14f5c29881de0bfa1ad880ae107b4b813e512d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0619a4034636e9c581d380d41dfa07fac2b499ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
